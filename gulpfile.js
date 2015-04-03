100665c8fe2721bc77c979467fd008349d06ee99
Tuomo Rissanen <tzrissan@gmail.com>
2015-04-03 11:24:21 +0300

    Watch-task for development ease

--- gulpfile.js
+++ gulpfile.js
@@ -3,27 +3,37 @@ var gulp = require('gulp'),
     del = require('del'),
     scp = require('gulp-scp');
 
+var path  = {
+	"app": {
+		"src": "app/*"
+	},
+	"build": {
+		"src": "build/*",
+		"target": "build"
+	}
+}
 
 
 gulp.task('clean', function(cb) {
-  del(['build'], cb);
+  del([path.build.target], cb);
 });
 
 gulp.task('package', function(cb) {
-	return gulp.src('app/*')
-		.pipe(gulp.dest('build'));
+	return gulp.src(path.app.src)
+			   .pipe(gulp.dest(path.build.target));
 });
 
 gulp.task('deploy', ['package'], function () {
-	
 	var config = require('./deploy-config.json');
+    gulp.src(path.build.src)
+        .pipe(scp({ host: config.host,
+		            user: config.user,
+		            path: config.targetdir
+		        }));
+});
 
-    gulp.src('build/*')
-        .pipe(scp({
-            host: config.host,
-            user: config.user,
-            path: config.targetdir
-        }));
+gulp.task('watch', function () {
+    gulp.watch(path.app.src, ['deploy']);
 });
 
 
