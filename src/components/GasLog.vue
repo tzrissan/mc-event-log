44b43542fb0db6b3534d4edd21804e8883b59300
Tuomo Rissanen <tuomo.rissanen@solita.fi>
2018-07-26 12:04:12 +0300

    Show only fuel-events here

--- src/components/GasLog.vue
+++ src/components/GasLog.vue
@@ -6,6 +6,7 @@
                 <th>pvm</th>
                 <th>odo</th>
                 <th>fuel</th>
+                <th>bike</th>
                 <th>info</th>
             </tr>
             </thead>
@@ -13,8 +14,12 @@
             <tr v-for="event in events" v-bind:key="event.bike + event.odo + event.type">
                 <td>{{ event.date | moment("d.M.YYYY") }}</td>
                 <td>{{ event.odo }} <small>km</small></td>
-                <td>{{ event.fuelused }} <small>ltr</small></td>
-                <td class="info">{{ events.info }}</td>
+                <td v-if="event.fuelused === event.fuelfilled">{{ event.fuelused }} <small>ltr</small></td>
+                <td v-if="event.fuelused !== event.fuelfilled">
+                    <span v-bind:title="'Tankattu ' + event.fuelfilled + ' ltr'">* {{ event.fuelused }} <small>ltr</small></span>
+                </td>
+                <td>{{ event.bike }}</td>
+                <td class="info">{{ event.info }}</td>
             </tr>
             </tbody>
         </table>
@@ -26,7 +31,9 @@
     export default {
         name: 'GasLog',
         data: function() {
-            return GasLogData.get();
+            return {
+                events: GasLogData.get().events.filter(event => event.type === 'FUEL')
+            };
         }
     }
 </script>
@@ -36,7 +43,7 @@
         border: 1px solid black;
         margin: 0;
         border-collapse: collapse;
-        text-align: left;
+        text-align: right;
     }
     tr:nth-child(even) {
         background: #EEE;
@@ -44,7 +51,7 @@
     th, td {
         margin: 0;
         padding: 0 0 0 5px;
-        white-space: pre;
+        white-space: nowrap;
     }
     th:last-child, td:last-child {
         padding: 0 15px 0 5px;
@@ -57,6 +64,7 @@
     }
     .info {
         white-space: pre-wrap;
+        text-align: left;
     }
     .gaslog {
         border: 1px solid black;
