16fdcca3887434f7054fce6dc49fd7fdae138d06
Tuomo Rissanen <tuomo.rissanen@solita.fi>
2018-07-26 14:56:12 +0300

    Kulutuslukemat

--- src/components/GasLog.vue
+++ src/components/GasLog.vue
@@ -1,27 +1,41 @@
 <template>
-        <table>
-            <thead>
-            <tr>
-                <th>pvm</th>
-                <th>odo</th>
-                <th>fuel</th>
-                <th>bike</th>
-                <th class="info">info</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="event in sortEvents(fuelEvents(events))" v-bind:key="event.bike + event.odo">
-                <td>{{ event.date | moment("D.M.YYYY") }}</td>
-                <td>{{ event.odo }} <small>km</small></td>
-                <td v-if="event.fuelused === event.fuelfilled">{{ event.fuelused }} <small>ltr</small></td>
-                <td v-if="event.fuelused !== event.fuelfilled">
-                    <span v-bind:title="'Tankattu ' + event.fuelfilled + ' ltr'">* {{ event.fuelused }} <small>ltr</small></span>
-                </td>
-                <td>{{ event.bike }}</td>
-                <td class="info">{{ event.info }}</td>
-            </tr>
-            </tbody>
-        </table>
+    <table>
+        <thead>
+        <tr>
+            <th>pvm</th>
+            <th>odo</th>
+            <th>matka</th>
+            <th>fuel</th>
+            <th>kulutus</th>
+            <th>bike</th>
+            <th class="info">info</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="event in sortEvents(fuelEvents(events))" v-bind:key="event.bike + event.odo">
+            <td>{{ event.date | moment("D.M.YYYY") }}</td>
+            <td>{{ event.odo }}
+                <small>km</small>
+            </td>
+            <td>{{ event.dist }}
+                <small v-if="event.dist">km</small>
+            </td>
+            <td v-if="event.fuelused === event.fuelfilled">{{ event.fuelused }}
+                <small>ltr</small>
+            </td>
+            <td v-if="event.fuelused !== event.fuelfilled">
+                <span v-bind:title="'Tankattu ' + event.fuelfilled + ' ltr'"><small>&#x26A0;</small> {{ event.fuelused }} <small>ltr</small></span>
+            </td>
+            <td class="milage">
+                <div class="unit" v-if="event.milage">litraa/<br/>100km</div>
+                <div class="amount">{{ event.milage }}</div>
+                <span v-if="!event.milage">-</span>
+            </td>
+            <td>{{ event.bike }}</td>
+            <td class="info">{{ event.info }}</td>
+        </tr>
+        </tbody>
+    </table>
 </template>
 
 <script>
@@ -30,12 +44,12 @@
 
     export default {
         name: 'GasLog',
-        data: function() {
+        data: function () {
             return GasLogData.get();
         },
         methods: {
             fuelEvents: (events) => events.filter(event => event.type === 'FUEL'),
-            sortEvents: (events, sort='date', asc=false) => {
+            sortEvents: (events, sort = 'date', asc = false) => {
                 const sorted = _.sortBy(events, sort);
                 return asc ? sorted : sorted.reverse();
             }
@@ -51,25 +65,42 @@
         text-align: right;
         width: 100%;
     }
+
     tr:nth-child(even) {
         background: #EEE;
     }
+
     th, td {
         margin: 0;
-        padding: 0 0 0 5px;
+        padding: 0 5px;
         white-space: nowrap;
     }
+
     th:last-child, td:last-child {
         padding: 0 15px 0 5px;
+        border-left: 1px solid black;
     }
-    th {
 
+    th {
         border-bottom: 1px solid black;
     }
-    td {
-    }
+
     .info {
         white-space: pre-wrap;
         text-align: left;
     }
+
+    .amount {
+        float: right;
+        clear: none;
+    }
+
+    .unit {
+        float: right;
+        clear: none;
+        font-size: 0.5em;
+        line-height: 0.9em;
+        padding-top: 0.1em;
+    }
+
 </style>
