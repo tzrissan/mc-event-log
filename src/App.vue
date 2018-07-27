5f0fca09c6c36e2e618a3cdd43e2d915409295e5
Tuomo Rissanen <tuomo.rissanen@solita.fi>
2018-07-27 17:28:31 +0300

    Tabs for simple navigation

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,79 @@
 <template>
     <div id="app">
-        <GasLog/>
+
+        <div class="tabs">
+            <div class="tab"
+                 v-bind:class="{ active: selectedTab === 'NewEvent'}"
+                 @click="selectedTab='NewEvent'">Uusi merkintä</div>
+            <div class="tab"
+                 v-bind:class="{ active: selectedTab === 'GasLog'}"
+                 @click="selectedTab='GasLog'">Tankkaukset</div>
+            <div class="tab"
+                 v-bind:class="{ active: selectedTab === 'Maintenance'}"
+                 @click="selectedTab='Maintenance'">Huollot</div>
+            <div class="tab"
+                 v-bind:class="{ active: selectedTab === 'Tyres'}"
+                 @click="selectedTab='Tyres'">Renkaat</div>
+            <div class="tab"
+                 v-bind:class="{ active: selectedTab === 'Seasons'}"
+                 @click="selectedTab='Seasons'">Kaudet</div>
+            <div class="tab"
+                 v-bind:class="{ active: selectedTab === 'Misc'}"
+                 @click="selectedTab='Misc'">Muut</div>
+        </div>
+        <div class="tab-content">
+            <GasLog v-if="selectedTab==='GasLog'"/>
+        </div>
     </div>
 </template>
 
 <script>
     import GasLog from './components/GasLog'
-    import ColumnSortIndicator from './components/ColumnSortIndicator'
-    import GasLogFilterSelector from './components/GasLogFilterSelector'
-    import FilterGlyph from './components/FilterGlyph';
 
     export default {
         name: 'app',
         components: {
-            GasLog,
-            GasLogFilterSelector,
-            ColumnSortIndicator,
-            FilterGlyph
-        }
+            GasLog
+        },
+        data: () => ({selectedTab: 'GasLog'})
     }
 </script>
 
-<style>
-    #app {
-        .blueSky {
-            color: #4CB5F5
-        }
-
-        .granite {
-            color: #B7B8B6
-        }
-
-        .pine {
-            color: #34675C
-        }
-
-        .fields {
-            color: #B3C100
-        }
+<style scoped>
+    div.tabs {
+        padding: 0;
+        border-bottom: 1px solid lightgray;
+        border-left: 1px solid black;
     }
+
+    div.tabs .tab {
+        display: inline-block;
+        border: 1px solid black;
+        border-bottom: none;
+        border-left: none;
+        padding: 10px 20px;
+        margin: 0;
+    }
+
+    .active {
+        color: white;
+        background-color: #4CB5F5;
+    }
+
+    .blueSky {
+        color: #4CB5F5
+    }
+
+    .granite {
+        color: #B7B8B6
+    }
+
+    .pine {
+        color: #34675C
+    }
+
+    .fields {
+        color: #B3C100
+    }
+
 </style>
