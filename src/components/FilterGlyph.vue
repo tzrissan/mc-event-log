34650d19be48a95cf523f559d19a65df097a7d3d
Tuomo Rissanen <tuomo.rissanen@solita.fi>
2018-07-27 16:48:10 +0300

    Resized

--- src/components/FilterGlyph.vue
+++ src/components/FilterGlyph.vue
@@ -1,7 +1,7 @@
 <template>
-    <svg height="20" width="20">
-        <polygon points="0,0 0,2 20,2 20,0" style="fill:black" />
-        <polygon points="0,3 8,13 8,20 12,17 12,13 20,3" style="fill:black" />
+    <svg height="15" width="15">
+        <polygon points="0,0 0,1 15,1 15,0" style="fill:black" />
+        <polygon points="0,2 5,8 5,13 10,15 10,8 15,2" style="fill:black" />
     </svg>
 </template>
 
