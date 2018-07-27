17721a395faa7a4d2f7e6b3c90dfd442d9fad682
Tuomo Rissanen <tuomo.rissanen@solita.fi>
2018-07-27 09:53:13 +0300

    Sorting by date

--- src/components/GasLog.vue
+++ src/components/GasLog.vue
@@ -1,57 +1,100 @@
 <template>
-    <table>
-        <thead>
-        <tr>
-            <th>pvm</th>
-            <th>odo</th>
-            <th>matka</th>
-            <th>fuel</th>
-            <th>kulutus</th>
-            <th>bike</th>
-            <th class="info">info</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr v-for="event in sortEvents(fuelEvents(events))" v-bind:key="event.bike + event.odo">
-            <td>{{ event.date | moment("D.M.YYYY") }}</td>
-            <td>{{ event.odo }}
-                <small>km</small>
-            </td>
-            <td>{{ event.dist }}
-                <small v-if="event.dist">km</small>
-            </td>
-            <td v-if="event.fuelused === event.fuelfilled">{{ event.fuelused }}
-                <small>ltr</small>
-            </td>
-            <td v-if="event.fuelused !== event.fuelfilled">
-                <span v-bind:title="'Tankattu ' + event.fuelfilled + ' ltr'"><small>&#x26A0;</small> {{ event.fuelused }} <small>ltr</small></span>
-            </td>
-            <td class="milage">
-                <div class="unit" v-if="event.milage">litraa/<br/>100km</div>
-                <div class="amount">{{ event.milage }}</div>
-                <span v-if="!event.milage">-</span>
-            </td>
-            <td>{{ event.bike }}</td>
-            <td class="info">{{ event.info }}</td>
-        </tr>
-        </tbody>
-    </table>
+    <div>
+        <table>
+            <thead>
+            <tr>
+                <th v-on:click="toggleSort('date')">pvm
+                    <span class="sort-arrow" v-if="local.sort=='date'">
+                        <span v-if="local.sortAsc">&darr;</span>
+                        <span v-if="!local.sortAsc">&uarr;</span>
+                    </span>
+                </th>
+                <th>odo</th>
+                <th>matka</th>
+                <th>fuel</th>
+                <th>kulutus</th>
+                <th>bike</th>
+                <th class="info">info <div class="settings" v-on:click="toggleShowFilters"><div/><div/><div/></div></th>
+            </tr>
+
+            </thead>
+            <tbody>
+            <tr v-for="event in sortEvents(fuelEvents(global.events), local.sort, local.sortAsc)" v-bind:key="event.bike + event.odo">
+                <td>{{ event.date | moment("D.M.YYYY") }}</td>
+                <td>{{ event.odo }}
+                    <small>km</small>
+                </td>
+                <td>{{ event.dist }}
+                    <small v-if="event.dist">km</small>
+                </td>
+                <td v-if="event.fuelused === event.fuelfilled">{{ event.fuelused }}
+                    <small>ltr</small>
+                </td>
+                <td v-if="event.fuelused !== event.fuelfilled">
+                    <span v-bind:title="'Tankattu ' + event.fuelfilled + ' ltr'"><small>&#x26A0;</small> {{ event.fuelused }} <small>ltr</small></span>
+                </td>
+                <td class="milage">
+                    <div class="unit" v-if="event.milage">litraa/<br/>100km</div>
+                    <div class="amount">{{ event.milage }}</div>
+                    <span v-if="!event.milage">-</span>
+                </td>
+                <td>{{ event.bike }}</td>
+                <td class="info">{{ event.info }}</td>
+            </tr>
+            </tbody>
+        </table>
+        <div class="filter-modal" v-if="local.showFilters">
+            <h1>Suodata</h1>
+            <div class="grid-container">
+                <div class="grid-item">Päivä</div>
+                <div class="grid-item">Kuukausi</div>
+                <div class="grid-item">Vuosi</div>
+                <div class="grid-item">Pyörä</div>
+                <div class="grid-item">Odo</div>
+                <div class="grid-item">Info</div>
+            </div>
+            <h1>Laske summat</h1>
+            <div class="grid-container">
+                <div class="grid-item">Päivä</div>
+                <div class="grid-item">Kuukausi</div>
+                <div class="grid-item">Vuosi</div>
+                <div class="grid-item">Pyörä</div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
     import _ from 'lodash';
     import GasLogData from '../data.js'
 
+    const local = {
+        showFilters: false,
+        sort: 'date',
+        sortAsc: false
+    };
+
     export default {
         name: 'GasLog',
         data: function () {
-            return GasLogData.get();
+            return {
+                local,
+                global: GasLogData.get()
+            };
         },
         methods: {
             fuelEvents: (events) => events.filter(event => event.type === 'FUEL'),
-            sortEvents: (events, sort = 'date', asc = false) => {
+            sortEvents: (events, sort, asc) => {
                 const sorted = _.sortBy(events, sort);
                 return asc ? sorted : sorted.reverse();
+            },
+            toggleShowFilters: () => { local.showFilters = !local.showFilters; },
+            toggleSort: (sort) => {
+                if (local.sort === sort) {
+                    local.sortAsc = !local.sortAsc;
+                } else {
+                    local.sort = sort;
+                }
             }
         }
     }
@@ -66,7 +109,11 @@
         width: 100%;
     }
 
-    tr:nth-child(even) {
+    thead {
+        background: #EEE;
+    }
+
+    tbody tr:nth-child(even) {
         background: #EEE;
     }
 
@@ -74,6 +121,7 @@
         margin: 0;
         padding: 0 5px;
         white-space: nowrap;
+        border-left: 1px solid lightgray;
     }
 
     th:last-child, td:last-child {
@@ -82,7 +130,8 @@
     }
 
     th {
-        border-bottom: 1px solid black;
+        text-align: center;
+        border: 1px solid black;
     }
 
     .info {
@@ -101,6 +150,44 @@
         font-size: 0.5em;
         line-height: 0.9em;
         padding-top: 0.1em;
+    }
+
+    .settings {
+        float: right;
+    }
+
+    .settings div {
+        width: 20px;
+        height: 3px;
+        background-color: black;
+        margin: 2px 0;
+    }
+
+    .filter-modal {
+        border: 2px solid black;
+        z-index: 50;
+        width: 80vw;
+        height: 80vh;
+        top: 10vh;
+        left: 10vw;
+        position: fixed;
+        background-color: white;
+        padding: 10px;
+    }
+
+    .grid-container {
+        display: grid;
+        grid-template-columns: auto auto auto;
+        grid-row-gap: 5px;
+        grid-column-gap: 5px;
+    }
+    .grid-item {
+        background-color: rgba(255, 255, 255, 0.8);
+        border: 1px solid rgba(0, 0, 0, 0.8);
+        text-align: center;
+    }
+    .sort-arrow {
+        font-weight: bold;
     }
 
 </style>
