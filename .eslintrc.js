20df59dacc04a82c1007d9cdb7220253ab49977b
Tuomo Rissanen <tuomo.rissanen@solita.fi>
2020-08-18 18:39:03 +0300

    More IDEA-approved lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,17 +10,17 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    "vue/script-indent": ["warn", 2, { "baseIndent": 1 }],
-    "spaced-comment": ["warn", "always"]
+    'vue/script-indent': ['warn', 2, { 'baseIndent': 0 }],
+    'spaced-comment': ['warn', 'always']
   },
   parserOptions: {
     parser: 'babel-eslint'
   },
-  "overrides": [
+  'overrides': [
     {
-      "files": ["*.vue"],
-      "rules": {
-        "indent": "off"
+      'files': ['*.vue'],
+      'rules': {
+        'indent': 'off'
       }
     }
   ]
