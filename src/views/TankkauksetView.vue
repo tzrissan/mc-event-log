7f144c79b00ee84b4e866901d41d026dc497bf0f
Tuomo Rissanen <tuomo.rissanen@iki.fi>
2022-06-30 10:43:23 +0300

    Tankkausten keskiarvojen visualisointeja

--- src/views/TankkauksetView.vue
+++ src/views/TankkauksetView.vue
@@ -24,19 +24,26 @@
         <td class="yksikko-km">{{ tankkaus.odo }} </td>
 
         <td v-if="tankkaus.matka">
-          <div v-if="matkanKeskiarvonLeveys" class="keskiarvo" v-bind:style="{ width: matkanKeskiarvonLeveys }" />
           <div class="yksikko-km kuvaaja" v-bind:style="{ width: matkanKuvaajanLeveys(tankkaus.matka) }">
-            {{ tankkaus.matka }}</div>
+            <div v-if="matkojenKeskiarvo < tankkaus.matka" class="keskiarvo"
+              v-bind:style="{ width: matkanKeskiarvonLeveys }"></div>
+            {{ tankkaus.matka }}
+          </div>
         </td>
         <td v-else />
 
         <td>
           <div class="yksikko-ltr kuvaaja" v-bind:style="{ width: bensanKuvaajanLeveys(tankkaus.bensa) }">
-            {{ desimaali(tankkaus.bensa) }}</div>
+            <div v-if="bensanKeskiarvo < tankkaus.bensa" class="keskiarvo"
+              v-bind:style="{ width: bensanKeskiarvonLeveys }"></div>
+            {{ desimaali(tankkaus.bensa) }}
+          </div>
         </td>
 
         <td v-if="tankkaus.kulutus !== undefined">
           <div class="kuvaaja" v-bind:style="{ width: kulutuksenKuvaajanLeveys(tankkaus.kulutus) }">
+            <div v-if="kulutuksenKeskiarvo < tankkaus.kulutus" class="keskiarvo"
+              v-bind:style="{ width: kulutuksenKeskiarvonLeveys }"></div>
             {{ desimaali(tankkaus.kulutus) }}
             <div class="yksikko-ltr-per-100-km"><br /></div>
           </div>
@@ -51,6 +58,44 @@
 <script lang="ts">
 import { store } from '../store.js';
 import moment from 'moment';
+import type { Tankkaus } from '@/schema.js';
+
+function matkanKuvaajanLeveys(matka: number): string {
+  return (matka / 4).toFixed(0) + 'px'
+};
+
+function bensanKuvaajanLeveys(bensa: number): string {
+  return (bensa * 4).toFixed(0) + 'px'
+};
+
+function kulutuksenKuvaajanLeveys(kulutus: number): string {
+  return (kulutus * 10).toFixed(0) + 'px'
+};
+
+function lueNumero(o: Tankkaus, k: keyof Tankkaus): number | undefined {
+  const value = o[k];
+  if (typeof value === 'number') {
+    return value;
+  }
+};
+
+function lueListastaNumerot(objects: Tankkaus[], key: keyof Tankkaus): number[] {
+  return objects
+    .map(o => lueNumero(o, key))
+    .filter(val => val !== undefined) as number[];
+};
+
+function keskiarvo(numerot: number[]): number {
+  // Tulkitaan kaikki puuttuvat arvot nolliksi
+  const yhteensa = numerot.reduce((a, b) => ((a || 0) + (b || 0)), 0);
+  const lukumaara = numerot.length;
+
+  if (lukumaara > 0) {
+    return yhteensa / lukumaara;
+  } else {
+    return 0;
+  }
+};
 
 export default {
   data() {
@@ -69,29 +114,31 @@ export default {
         return numero.toFixed(2);
       }
     },
-    matkanKuvaajanLeveys(matka: number): string {
-      return (matka / 4).toFixed(0) + 'px'
-    },
-    bensanKuvaajanLeveys(bensa: number): string {
-      return (bensa * 4).toFixed(0) + 'px'
-    },
-    kulutuksenKuvaajanLeveys(kulutus: number): string {
-      return (kulutus * 10).toFixed(0) + 'px'
-    },
+    matkanKuvaajanLeveys,
+    bensanKuvaajanLeveys,
+    kulutuksenKuvaajanLeveys,
   },
   computed: {
+    matkojenKeskiarvo(): number {
+      const matkat = lueListastaNumerot(store.tankkaukset, 'matka')
+      return keskiarvo(matkat)
+    },
     matkanKeskiarvonLeveys(): string {
-      const matkat = store.tankkaukset
-        .map(tankkaus => tankkaus.matka)
-        .filter(matka => matka !== undefined)
-      const matkaYhteensa = matkat.reduce((a, b) => ((a || 0) + (b || 0)), 0);
-
-      if (matkaYhteensa && matkat.length > 0) {
-        return this.matkanKuvaajanLeveys(matkaYhteensa / matkat.length);
-      } else {
-        return '0px';
-      }
-
+      return matkanKuvaajanLeveys(this.matkojenKeskiarvo);
+    },
+    bensanKeskiarvo(): number {
+      const bensat = lueListastaNumerot(store.tankkaukset, 'bensa')
+      return keskiarvo(bensat)
+    },
+    bensanKeskiarvonLeveys(): string {
+      return bensanKuvaajanLeveys(this.bensanKeskiarvo);
+    },
+    kulutuksenKeskiarvo(): number {
+      const bensat = lueListastaNumerot(store.tankkaukset, 'kulutus')
+      return keskiarvo(bensat)
+    },
+    kulutuksenKeskiarvonLeveys(): string {
+      return kulutuksenKuvaajanLeveys(this.kulutuksenKeskiarvo);
     }
   }
 }
@@ -100,7 +147,7 @@ export default {
 <style scoped>
 .kuvaaja {
   border: none;
-  border-right: 2px solid rgba(235, 235, 235, 1);
+  border-right: 2px solid rgba(151, 151, 151, 0.1);
   background-color: rgba(235, 235, 235, 0.64);
   overflow: visible;
   margin: 0.2em 0;
@@ -110,8 +157,7 @@ export default {
 
 .keskiarvo {
   border: none;
-  border-right: 1px solid rgba(49, 76, 228, 0.1);
-  width: 50px;
+  border-right: 2px solid rgba(151, 151, 151, 0.1);
   height: 100%;
   z-index: 1;
   position: absolute;
